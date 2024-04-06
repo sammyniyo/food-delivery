70efc4947d184a77180cb1d6ce5efcc9d08dc67a
sammyniyo <samshakul@gmail.com>
2024-04-06 21:14:09 +0200

    working on context

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import './ExploreMenu.css'
-import { menu_list } from '../../assets/assets'
+import React, { useState } from 'react';
+import './ExploreMenu.css';
+import { menu_list } from '../../assets/assets';
 
-const  ExploreMenu = ({category,setCategory}) =>{
+const ExploreMenu = ({ category, setCategory }) => {
   return (
     <div className='explore-menu' id='explore-menu'>
-        <h1>Explore our menu</h1>
-        <p>Whether you crave the comforting embrace of home-cooked classics or the daring allure of avant-garde creations, our kitchen is your canvas, painting culinary dreams into reality.</p>
-        <div className='explore-menu-list'>
-            {menu_list.map((item,index)=>{
-                return(
-                    <div onClick={()=>setCategory(prev===item.menu_name?"All":item.menu_name)} key={index} className='explore-menu-list-item'>
-                        <img className={category===item.menu_name?"active":""} src={item.menu_image} alt={item.menu_name} />
-                        <p>{item.menu_name}</p>
-                    </div>
-                )
-            })}
-        </div>
-        <hr />
+      <h1>Explore our menu</h1>
+      <p>Whether you crave the comforting embrace of home-cooked classics or the daring allure of avant-garde creations, our kitchen is your canvas, painting culinary dreams into reality.</p>
+      <div className='explore-menu-list'>
+        {menu_list.map((item, index) => {
+          return (
+            <div onClick={() => setCategory(prev => prev === item.menu_name ? "All" : item.menu_name)} key={index} className='explore-menu-list-item'>
+              <img className={category === item.menu_name ? "active" : ""} src={item.menu_image} alt={item.menu_name} />
+              <p>{item.menu_name}</p>
+            </div>
+          );
+        })}
+      </div>
+      <hr />
     </div>
-  )
-}
+  );
+};
 
-export default ExploreMenu
+export default ExploreMenu;
